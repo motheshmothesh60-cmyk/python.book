c54bf32929ff11c6f361b0d0787b090c8d495f69
motheshmothesh60-cmyk <motheshmothesh60@gmail.com>
2026-05-18 15:07:56 +0530

    Add files via upload

---
+++ pythan2.py
@@ -0,0 +1,11 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+===== RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python314/tg.py ====
+Enter the number:76
+reversed number: 67
+>>> 
+==== RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python314/hhy.py ====
+Enter number43
+totai digits: 1
+totai digits: 2
